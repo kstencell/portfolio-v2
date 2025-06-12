e0f9a9ea896d3d9f5721937032401f9f0a243c1a
Karl Stencell <karldstencell@gmail.com>
2025-06-11 21:52:55 -0400

    changed favicon stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,11 +19,7 @@ export const metadata = {
   description:
     "Portfolio website for Karl Stencell, software engineer and automation enthusiast.",
   icons: {
-    icon: [
-      { url: "/favicon.ico", sizes: "any" }, // default
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    ],
+    icon: [{ url: "/favicon/favicon.ico", sizes: "any" }],
     apple: "/apple-touch-icon.png",
   },
   manifest: "/site.webmanifest",
